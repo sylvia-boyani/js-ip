cacd654325151d58f5546f7f6cbc6a754c46a798
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-05-15 07:43:17 +0300

    still working on functions

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,9 @@ function check() {
     var qn4=document.quiz.qn4.value;
     var qn5=document.quiz.qn5.value;
     if (qn1=="variables") {b++}
-    if (qn2=="variables") {a++}
-    if (qn3=="variables") {d++}
-    if (qn4=="variables") {a++}
-    if (qn5=="variables") {d++}
+    if (qn2=="hypertext markup language") {a++}
+    if (qn3=="capitalizing the first letter of the second or third word") {d++}
+    if (qn4=="1") {a++}
+    if (qn5=="objects") {d++}
     document.write(b)
 }
