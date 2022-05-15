3b49eae7d87b2d610870b4a679fdb6af060ed483
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-05-15 07:28:41 +0300

    first function responsive

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 function check() {
     var b=0;
     var qn1=document.quiz.qn1.value;
+    var qn2=document.quiz.qn2.value;
+    var qn3=document.quiz.qn3.value;
+    var qn4=document.quiz.qn4.value;
+    var qn5=document.quiz.qn5.value;
     if (qn1=="variables") {b++}
     document.write(b)
 }
